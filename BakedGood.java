a3009c3614180fae8bbbff7ea7201af19262c50a
amelia <az111@wellesley.edu>
2024-01-16 10:47:21 -0500

    added main method, toString, increaseQuantity

--- BakedGood.java
+++ BakedGood.java
@@ -24,4 +24,24 @@ public class BakedGood
         this.price = price;
         this.quantity = quantity; 
     }
+    
+    //modify quantity
+    public void increaseQuantity(int count) {
+        this.quantity += count;
+    }
+    
+    //toString
+    public String toString() {
+        return "baked good: " + this.name + " (" + this.quantity + " @$" + this.price + ")"; 
+    }
+    
+    public static void main(String[] args) {
+        BakedGood croissant = new BakedGood("croissant", "butter, flour, laminate, yum", 20.0, 0);
+        //bake a dozen croissants
+        croissant.increaseQuantity(12); 
+        System.out.println(croissant);
+        //bake 3 more croissants with the leftover dough
+        croissant.increaseQuantity(3);
+        System.out.println(croissant); 
+    }
 }
